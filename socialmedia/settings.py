bfade704bcf2302e1d2988423b4729eb901538b6
Daniel Msuthwana <danray78699@gmail.com>
2026-04-03 11:19:30 +0200

    Updated settings, apps config and admin setup

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-3+&3op^4s(zn+4ee-cg7vf9bz%e=nxm0-55!6r*yc^6jxf!%sp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-
+AUTH_USER_MODEL = 'accounts.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'posts',
+    'messaging',
+    'social',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
