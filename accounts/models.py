3df71dd2afd079f7c154f30bdfeba3e8048c4468
Daniel Msuthwana <danray78699@gmail.com>
2026-04-07 14:58:57 +0200

    Added Modes

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    bio =  models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(
+        upload_to = 'profile_pics',
+        blank = True,
+        null = True,
+    )
+#Demostrate
+ROLE_CHOICES = [
+    ('user', 'User')
+    ('admin', 'Admin')
+]
+
+def __str___(self):
+    return self.username
