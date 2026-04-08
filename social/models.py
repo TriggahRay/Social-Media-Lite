4079152c87905da4ae83e247a0da49487655db1d
KANI <kanithutomokoena@gmail.com>
2026-04-08 13:05:40 +0200

    Added Models and Views

--- social/models.py
+++ social/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Post(models.Model):
+    body = models.TextField() # user types in textbox to create a post
+    created_on = models.DateTimeField(default=timezone.now) # the time the post was created/submitted
+    author = models.ForeignKey(AbstractUser, on_delete=models.CASCADE) # finds a user currently logged in
--- social/views.py
+++ social/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Post
 
 # Create your views here.
+class PostListView(View):
+    def get(self, request, *args, **kwargs): # will run whenevr the user tries to ciew this page
+        posts = Post.objects.all().order_by('-created_on') # displays posts from the most recent to the last (newest to oldest)
+
+        context = {
+            'post_list' : posts,
+        }
+        
+        return render(request, 'social/post_list.html', context)
