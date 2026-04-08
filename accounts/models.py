c76df707f91f00cd74358693327574ec2b0fa4a9
Daniel Msuthwana <danray78699@gmail.com>
2026-04-08 11:03:46 +0200

    Updated the Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,30 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-
-# Create your models here.
 class User(AbstractUser):
-    bio =  models.TextField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
     profile_pic = models.ImageField(
-        upload_to = 'profile_pics',
-        blank = True,
-        null = True,
+        upload_to='profile_pics/', 
+        blank=True, 
+        null=True, 
+        default='profile_pics/default.png'
     )
-#Demostrate
-ROLE_CHOICES = [
-    ('user', 'User')
-    ('admin', 'Admin')
-]
+    ROLE_CHOICES = [
+        ('user', 'User'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
 
-def __str___(self):
-    return self.username
+    def __str__(self):
+        return self.username
+
+    def get_followers_count(self):
+        return self.followers.count()
+
+    def get_following_count(self):
+        return self.following.count()
+
+    def get_posts_count(self):
+        return self.posts.count()
